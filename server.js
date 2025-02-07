115922cb01c072f2763d0efd15c11b46aaed3335
MakramElghadir <MkElghadir@gmail.com>
2025-02-07 10:22:33 +0100

    added cors in server.js

--- server.js
+++ server.js
@@ -1,49 +1,52 @@
-require("dotenv").config()
+require("dotenv").config();
 const express = require('express');
 const fs = require('fs');
 const https = require('https');
 const http = require('http');
+const cors = require('cors'); // Import cors package
 const app = express();
 app.use(express.json());
-const {getMessages, addMessage} = require('./database.js');
+const { getMessages, addMessage } = require('./database.js');
 const path = require('path');
 
 const APIKEY = "123456";
 
+// Enable CORS for all origins (adjust as needed)
+app.use(cors());
+
+// Default route
 app.get('/', (req, res) => {
   res.send('Bienvenido');
-})
+});
 
-app.get('/message', (req, res) => {    
-   // Devolver mensajes alamacenados en la BBDD
-   const apikey = req.headers['apikey'];
-   if (apikey !== APIKEY){
-     return res.status(401).send('Unauthorized');
-    }else if (apikey === APIKEY){
-      res.json(getMessages());
-      return res.status(200).send('OK');  
-   }
-})
+// Route for fetching messages
+app.get('/message', (req, res) => {
+  const apikey = req.headers['apikey'];
+  if (apikey !== APIKEY) {
+    return res.status(401).send('Unauthorized');
+  } else if (apikey === APIKEY) {
+    res.json(getMessages());
+    return res.status(200).send('OK');
+  }
+});
 
-app.post('/message', (req, res) => {    
-   // Guardar mensajes en la BBDD
-   const apikey = req.headers['apikey'];
+// Route for posting messages
+app.post('/message', (req, res) => {
+  const apikey = req.headers['apikey'];
   if (apikey !== APIKEY) {
     return res.status(401).send('Unauthorized');
   }
 
-  // Manda el mensaje por la query y lo añadade a la BBDD
-  const message = req.body.message;  
+  const message = req.body.message;
   if (message) {
     addMessage(message);
     res.status(201).send('Message added');
   } else {
     res.status(400).send('Bad Request');
   }
-})
+});
 
-
-
+// Start HTTPS or HTTP server
 if (process.env.NODE_ENV === 'production') {
   const options = {
     key: fs.readFileSync(path.join(__dirname, 'privkey.pem')),
@@ -53,8 +56,7 @@ if (process.env.NODE_ENV === 'production') {
   https.createServer(options, app).listen(3000, () => {
     console.log('Server started on https://dev4.cyberbunny.online:3000');
   });
-}
-else {
+} else {
   http.createServer(app).listen(3000, () => {
     console.log('Server started on http://dev4.cyberbunny.online:3000');
   });
